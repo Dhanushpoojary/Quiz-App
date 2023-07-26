03a5752aafe3fe95f3aab4664419db0fd0509355
Dhanush Poojary <dhanushpoojary062@gmail.com>
2023-07-26 23:08:17 +0530

    commit

---
+++ javascript1.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "In which one of the following states is “Gir forest” located?",
+    answer: "Gujarat",
+    options: [
+      "Maharashtra",
+      "Rajasthan",
+      "Haryana",
+      "Gujarat"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The forest in Sundarbans is also called?",
+    answer: "Mangrove",
+    options: [
+      "Scrub jungle",
+      "Mangrove",
+      "Deciduous forest",
+      "None of these"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Name the tropical evergreen forest in India.",
+    answer: "The Western Ghats ",
+    options: [
+      "The Western Ghats ",
+      "Semiarid areas in Gujarat",
+      "Madhya Pradesh",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the animal symbol of W. W. F (World Wildlife Fund) ?",
+    answer: "Gaint Panda",
+    options: [
+      "Red Panda",
+      "Gaint Panda",
+      "Tiger",
+      "Kangaroo"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which one of the below is a manmade forest in India?",
+    answer: "The Nandini forest",
+    options: [
+      "Wayanad Wildlife Sanctuary",
+      "The Nandini forest",
+      "Van vihar",
+      "None of these"
+    ]
+  },
+ 
+];
